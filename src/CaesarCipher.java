b1aafac4f02f94779eb530b9d4b73c232ad9efb3
Kirill <zalogi94@gmail.com>
2023-11-24 22:43:29 +0300

    Удалил комменты из кода Удалил Main из кода Дополнили и расширили Alphabet

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,32 +1,17 @@
 public class CaesarCipher {
-    private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    /**
-     * Мы делаем то-то и то-то
-     *
-     * @param message
-     * @param key
-     * @return
-     */
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—" + "0123456789";
 
     public String encrypt(String message, int key) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < message.length(); i++) {
             char symbol = message.charAt(i);
-            int index = alphabet.indexOf(symbol);
+            int index = ALPHABET.indexOf(symbol);
             if (index >= 0) {
-                int newIndex = (index + key) % alphabet.length();
-/*                char charAt = 0;
-                if (newIndex < 0) {
-                    charAt = alphabet.charAt(newIndex+alphabet.length());
-                } else {
-                    charAt = alphabet.charAt(newIndex);
-                }
-                charAt = newIndex < 0 ? alphabet.charAt(newIndex+alphabet.length()) : alphabet.charAt(newIndex); //Вариант 2
-                charAt = alphabet.charAt(newIndex < 0 ? newIndex+alphabet.length() : newIndex); // Вариант 3
-*/
-                char charAt = alphabet.charAt(newIndex + (newIndex < 0 ? alphabet.length() : 0)); //Вариант 4
-                builder.append(charAt); //билдер это строка которая может меняться, а обычная строка не может меняться, билдер - мутабельный.
+                int newIndex = (index + key) % ALPHABET.length();
+                char charAt = ALPHABET.charAt(newIndex + (newIndex < 0 ? ALPHABET.length() : 0));
+                builder.append(charAt);
             }
         }
         return builder.toString();
@@ -34,16 +19,6 @@ public class CaesarCipher {
 
     public String decrypt(String message, int key) {
         return encrypt(message, key * -1);
-    }
-
-    public static void main(String[] args) {
-        CaesarCipher caesarCipher = new CaesarCipher();
-        String encrypt = caesarCipher.encrypt("privet", 3);
-
-        System.out.println(encrypt);
-
-        String decrypt = caesarCipher.decrypt(encrypt, 3);
-        System.out.println(decrypt);
     }
 }
 
