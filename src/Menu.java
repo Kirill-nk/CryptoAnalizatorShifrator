9091edf02919673026690bd35cd1bc681f391746
Kirill <zalogi94@gmail.com>
2023-11-24 22:37:25 +0300

    Реализовали класс меню, реализовали класс цезарь и проверили что он работает

---
+++ src/Menu.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Menu {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("""
+                    Выберите действие введя его номер\s
+                    1. Зашифровать текст из файла\s
+                    2. Расшифровать тект из файла\s
+                    3. Подобрать ключ к зашифрованному файлу\s
+                    4. Расшифровать файл используя синтаксический анализ\s
+                    5. Выйти из программы""");
+            String answer = input.readLine();
+            switch (answer) {
+                case "1" -> System.out.println("1. Зашифровать текст из файла");
+                case "2" -> System.out.println("2. Расшифровать тект из файла");
+                case "3" -> System.out.println("3. Подобрать ключ к зашифрованному файлу");
+                case "4" -> System.out.println("4. Расшифровать текст из файла");
+                case "5" -> {
+                    return;
+                }
+            }
+        }
+    }
+}
