56d8de9f6a00fd8a158573708bca7652c08a4805
Kirill-nk <kirill9414@yandex.ru>
2024-01-14 23:54:44 +0300

    Добавил класс BrooteForce, в котором придается путь к файлу для зашифровки-расшифровки

--- src/main/java/BruteForce.java
+++ src/main/java/BruteForce.java
@@ -1,3 +1,21 @@
-public class BruteForce {
+import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class BruteForce {
+@SneakyThrows //подавляет checked Exeption в unchecked
+    public void bruteForce() {
+        Util.writeMessage("Введите адрес к файлу для его расшифровки: ");
+        String src = Util.readString();
+        Path dest = Util.buildFileName(src, "_bruteForce");
+
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(src));
+             BufferedWriter writer = Files.newBufferedWriter(dest)) {
+
+        }
+    }
 }
