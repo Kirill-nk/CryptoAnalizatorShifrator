2eb3b01bb05b0cacd01a765dba7fa684c81140d4
Kirill-nk <kirill9414@yandex.ru>
2024-03-21 22:39:39 +0300

    Parsing, дописан.

--- src/main/java/Parsing.java
+++ src/main/java/Parsing.java
@@ -9,23 +9,45 @@ import java.nio.file.Path;
 import java.security.Key;
 import java.util.*;
 
-
 public class Parsing {
+    @SneakyThrows
     public void parse() {
-
         Util.writeMessage("Введите адрес файла для его расшифровки: ");
         String src = Util.readString();
         Util.writeMessage("Введите адрес файла для набора статистики: ");
         String statisticSrc = Util.readString();
 
+        Path parsing = Util.buildFileName(src, "_parse");
+
         List<Map.Entry<Character, Integer>> listEncrypted = convertToList(src);
         List<Map.Entry<Character, Integer>> listStatistic = convertToList(statisticSrc);
 
-        Map<Character, Character> decrypted = new HashMap<>();
+        if (listEncrypted.size() <= listStatistic.size()) {
+            Map<Character, Character> decrypted = new HashMap<>();
+            for (int i = 0; i < listEncrypted.size(); i++) {
+                decrypted.put(listEncrypted.get(i).getKey(), listStatistic.get(i).getKey());
+
+            }
+            try (BufferedReader reader = Files.newBufferedReader(Path.of(src));
+                 BufferedWriter writer = Files.newBufferedWriter(parsing);
+            ) {
+                while (reader.ready()) {
+                    String string = reader.readLine();
+                    StringBuilder builder = new StringBuilder();
+                    for (char aChar : string.toCharArray()) {
+                       Character decryptedChar = decrypted.get(aChar);
+                       builder.append(decryptedChar);
+                    }
+                    writer.write(builder.toString());
+                    writer.newLine();
+                }
+            }
+            Util.writeMessage("Содержимое файла расшифровано");
+        } else {
+            Util.writeMessage("Размер файла статистики недостаточен для расшифровки, необходим файл большего размера.");
+        }
+
     }
-
-
-
 
     @SneakyThrows
     private List<Map.Entry<Character, Integer>> convertToList(String path) {
